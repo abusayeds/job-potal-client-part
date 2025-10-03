90c361f768f0746dd91e45768d2abe3accc570ad
abusayeds <abusayedstudent855@gmail.com>
2025-10-03 10:09:08 +0600

    remove unnecessary pages

--- src/app/(auth)/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { TLayoutProps } from "@/types";
-
-const AuthLayout = ({ children }: TLayoutProps) => {
-  return <div>{children}</div>;
-};
-
-export default AuthLayout;
--- src/utils/copyToClipBoard.ts
+++
@@ -1,9 +0,0 @@
-import { errorAlert, TResError } from "@/lib/alerts";
-
-export const copyToClipboard = async (text: string) => {
-    try {
-        await navigator.clipboard.writeText(text);
-    } catch (error) {
-        errorAlert({ error: error as TResError });
-    }
-};
