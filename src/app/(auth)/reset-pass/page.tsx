757ce2d89ebc47a7d390b4d41f75949205c2239d
abusayeds <abusayedstudent855@gmail.com>
2026-06-25 23:57:13 +0600

    update 2

--- src/app/(auth)/reset-pass/page.tsx
+++ src/app/(auth)/reset-pass/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import ResetPass from "@/components/Auth/ResetPass";
 
 const page = () => {
+  
   return (
     <Container>
       <div className="relative w-full max-w-48 2xl:max-w-52 aspect-[4/1] mx-auto">
